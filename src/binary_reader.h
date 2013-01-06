d4c44f9083b6ef3d5d3691250ddde185cbd4c255
Matthias Beyer <matthias@ib-fb.de>
2013-01-06 20:39:29 +0100

    added some function prototypes and includes

--- src/binary_reader.h
+++ src/binary_reader.h
@@ -1,9 +1,15 @@
 #ifndef __MINX_BINARY_READER_H__
 #define __MINX_BINARY_READER_H__
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-uint16_t    minx_binary_get_at(uint64_t p, unsigned int number_of_bytes);
-signed int  minx_binary_exists_at(uint64_t p);
+#include "error.h"
+
+void            minx_binary_init        ( FILE *f );
+void            minx_binary_shutdown    (void);
+void *          minx_binary_get_at      (uint64_t p, unsigned int number_of_bytes);
+signed int      minx_binary_exists_at   (uint64_t p);
 
 #endif // __MINX_BINARY_READER_H__
