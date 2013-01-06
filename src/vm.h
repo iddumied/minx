0ed6b415c3468a52b82fbafe70c8cb1407b81814
Matthias Beyer <matthias@ib-fb.de>
2013-01-06 16:01:47 +0100

    added header for vm

--- src/vm.h
+++ src/vm.h
@@ -1,6 +1,24 @@
 #ifndef __MINX_VM_H__
 #define __MINX_VM_H__
 
-#include "vm_defs.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "binary_reader.h"
+#include "vm_commands.h"
+#include "config.h"
+#include "error.h"
+
+#include "stack/stack.h"
+
+/* defines for size in bytes */
+#define COMMAND_SIZE    2
+#define VALUE_SIZE      8
+#define ADDRESS_SIZE    8
+
+typedef struct {
+    uint64_t * value;
+} Register;
 
 #endif //__MINX_VM_H__
