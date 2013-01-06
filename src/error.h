f5b06012c3eaa638848b20111d8cc2cae92c9a20
Matthias Beyer <matthias@ib-fb.de>
2013-01-06 18:50:53 +0100

    added more information to error macros

--- src/error.h
+++ src/error.h
@@ -4,15 +4,18 @@
 #include <stdio.h>
 
 #define FATAL_ERROR         do {                                            \
-                                printf("minx: FATAL ERROR. Aborting\n");    \
+                                printf( "minx: FATAL ERROR.\n"              \
+                                        "func %s\nline %s\n"                \
+                                        "Aborting\n", __func__,__LINE__);   \
                                 exit(1);                                    \
                             } while (0);
 
 #define FATAL_DESC_ERROR(desc)  do {                                        \
                                     printf( "minx: FATAL ERROR.\n"          \
+                                            "func %s\nline %s\n"            \
                                             "%s\n"                          \
                                             "Aborting.\n"                   \
-                                            ,desc);                         \
+                                            ,__func__,__LINE__,desc);       \
                                     exit(1);                                \
                                 } while (0);
 
