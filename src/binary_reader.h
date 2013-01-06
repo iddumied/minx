5c0ba56838b9883f3c281e8a82dc0b4513baa17e
Matthias Beyer <matthias@ib-fb.de>
2013-01-06 16:04:10 +0100

    added header for binary-reader

---
+++ src/binary_reader.h
@@ -0,0 +1,8 @@
+#ifndef __MINX_BINARY_READER_H__
+#define __MINX_BINARY_READER_H__
+
+#include <stdint.h>
+
+uint16_t    minx_consume_bytes(unsigned int bytes_count);
+
+#endif // __MINX_BINARY_READER_H__
