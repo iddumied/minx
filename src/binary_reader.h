dc5f6bf78453b77951cddc1bb28ad4ae12b3e65b
Matthias Beyer <matthias@ib-fb.de>
2013-01-06 18:50:40 +0100

    added missing function prototype

--- src/binary_reader.h
+++ src/binary_reader.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 uint16_t    minx_binary_get_at(uint64_t p, unsigned int number_of_bytes);
+signed int  minx_binary_exists_at(uint64_t p);
 
 #endif // __MINX_BINARY_READER_H__
