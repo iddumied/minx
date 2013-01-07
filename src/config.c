ccad2ac7e802912a52ec3c3028c75dab0a8ce8d4
Matthias Beyer <matthias@ib-fb.de>
2013-01-07 15:37:57 +0100

    added config.c and some function prototypes

---
+++ src/config.c
@@ -0,0 +1,10 @@
+#include "config.h"
+
+static Map * config_map;
+
+void minx_config_parse(unsigned int len, char *str) {
+}
+
+void * minx_config_get(ConfigurationType ct) {
+
+}
--- src/config.h
+++ src/config.h
@@ -1,12 +1,15 @@
 #ifndef __MINX_CONFIG_H__
 #define __MINX_CONFIG_H__
 
+#include "map/map.h"
+
 typedef enum {
     CONF_REGISTER_SIZE,
     CONF_ADDRESS_SIZE,
 
 } ConfigurationType;
 
+void        minx_config_parse   (unsigned int len, char *str);
 void *      minx_config_get     (ConfigurationType ct);
 
 #endif // __MINX_CONFIG_H__
