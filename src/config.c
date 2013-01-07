7a7ded9e0ef50520a93593b8b39af24c594999b8
Matthias Beyer <matthias@ib-fb.de>
2013-01-07 18:29:13 +0100

    rewrote config.{c,h}, removed options.{c,h}

--- src/config.c
+++ src/config.c
@@ -1,10 +1,36 @@
 #include "config.h"
 
-static Map * config_map;
+/*
+ * Static variables
+ */
+static Configuration    configuration[] = {
+    {   .type = VERBOSITY,  .value.i = 0  },
+};
 
+static unsigned int     number_of_confs;
+
+/*
+ * Functions
+ */
+
+void minx_config_init() {
+    number_of_confs = (unsigned int) sizeof(configuration)/sizeof(configuration[0]);
+}
+
+void minx_config_shutdown() {
+    return;
+}
+
+/*
+ *
+ */
 void minx_config_parse(unsigned int len, char *str) {
 }
 
 void * minx_config_get(ConfigurationType ct) {
-
+    unsigned int i;
+    for(i = 0 ; i < number_of_confs; i++)
+        if(configuration[i].type == ct)
+            return &configuration[i].value;
+    return NULL;
 }
--- src/config.h
+++ src/config.h
@@ -1,11 +1,23 @@
 #ifndef __MINX_CONFIG_H__
 #define __MINX_CONFIG_H__
 
-#include "map/map.h"
+#include <stdlib.h>
+#include <string.h>
 
 typedef enum {
-
+    VERBOSITY,
 } ConfigurationType;
+
+typedef union {
+    int b;
+    int i;
+    char *str;
+} ConfigurationValue;
+
+typedef struct {
+    ConfigurationType       type;
+    ConfigurationValue      value;
+} Configuration;
 
 void        minx_config_parse   (unsigned int len, char *str);
 void *      minx_config_get     (ConfigurationType ct);
