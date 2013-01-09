190f1c53dfc3736f427e86ace7a2beb35fd8109e
Matthias Beyer <matthias@ib-fb.de>
2013-01-09 20:56:50 +0100

    added simple_reader
    simple_reader is a simple implementation of the binary_reader. It's not
    supposed to run in a stable system, it's for testing the VM.

---
+++ src/simple_reader.c
@@ -0,0 +1,49 @@
+#include "binary_reader.h"
+
+/*
+ * Static variables
+ */ 
+
+static char * binary;
+static long filesize;
+
+/*
+ * Static function prototypes
+ */
+
+static long get_file_size(FILE *f);
+
+/*
+ * Functions
+ */
+
+void minx_binary_init(FILE *f) {
+    filesize = get_file_size(f);
+    binary = (char*) malloc( sizeof(char) * filesize );
+} 
+
+void minx_binary_shutdown() {
+    free(binary);
+}
+
+void * minx_binary_get_at(uint64_t p, unsigned int number_of_bytes) {
+    char *res = malloc( sizeof(char) * number_of_bytes );
+    memcpy(res, &binary[p], number_of_bytes);
+    return res;
+}
+
+signed int minx_binary_exists_at(uint64_t p) {
+    return filesize > p;
+}
+
+/*
+ * Static functions implementations
+ */
+
+static long get_file_size(FILE *f) {
+    long res;
+    fseek(f, 0L, SEEK_END);
+    res = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+    return res;
+}
