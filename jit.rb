62bfb05534b8bb070c3ace1c56f61c1b6bdbbc89
Matthias Beyer <matthias@ib-fb.de>
2013-01-10 20:25:52 +0100

    improved and fixed jit

--- jit.rb
+++ jit.rb
@@ -1,35 +1,55 @@
+REGISTER  = 2
+VALUE     = 8
+ADDRESS   = 8
+
+class Op < Struct.new :opc, :args; end
+
 @ops = { 
-  "NOP" =>    0x00,
-  "CALL" =>   0x01,
-  "RET" =>    0x02,
-  "MOV" =>    0x03,
-  "MOVI" =>   0x04,
-  "NOT" =>    0x05,
-  "NOTR" =>   0x06,
-  "AND" =>    0x07,
-  "ANDI" =>   0x08,
-  "ANDR" =>   0x09,
-  "ANDIR" =>  0x0A,
-  "OR" =>     0x0B,
-  "ORI" =>    0x0C,
-  "ORR" =>    0x0D,
-  "ORIR" =>   0x0E,
-  "DEC" =>    0x0F,
-  "INC" =>    0x10,
-  "LSH" =>    0x11,
-  "RSH" =>    0x12,
-  "PUSH" =>   0x20,
-  "POP" =>    0x21,
-  "DROP" =>   0x22,
-  "ADD" =>    0x30,
-  "ADDI" =>   0x31,
-  "ADDR" =>   0x32,
-  "ADDIR" =>  0x33,
-  "JMP" =>    0x40,
-  "JMPIZ" =>  0x41,
-  "JMPNZ" =>  0x42,
-  "IFZJMP" => 0x43,
+  "NOP" =>    Op.new(0x00, [] ), 
+  "CALL" =>   Op.new(0x01, [ADDRESS] ), 
+  "RET" =>    Op.new(0x02, [] ), 
+  "MOV" =>    Op.new(0x03, [REGISTER, REGISTER] ), 
+  "MOVI" =>   Op.new(0x04, [REGISTER, VALUE] ), 
+  "NOT" =>    Op.new(0x05, [REGISTER] ), 
+  "NOTR" =>   Op.new(0x06, [REGISTER] ), 
+  "AND" =>    Op.new(0x07, [REGISTER, REGISTER] ), 
+  "ANDI" =>   Op.new(0x08, [REGISTER, VALUE] ), 
+  "ANDR" =>   Op.new(0x09, [REGISTER, REGISTER] ), 
+  "ANDIR" =>  Op.new(0x0A, [REGISTER, VALUE] ), 
+  "OR" =>     Op.new(0x0B, [REGISTER, REGISTER] ), 
+  "ORI" =>    Op.new(0x0C, [REGISTER, VALUE] ), 
+  "ORR" =>    Op.new(0x0D, [REGISTER, REGISTER] ), 
+  "ORIR" =>   Op.new(0x0E, [REGISTER, VALUE] ), 
+  "DEC" =>    Op.new(0x0F, [REGISTER] ), 
+  "INC" =>    Op.new(0x10, [REGISTER] ), 
+  "LSH" =>    Op.new(0x11, [REGISTER] ), 
+  "RSH" =>    Op.new(0x12, [REGISTER] ), 
+  "PUSH" =>   Op.new(0x20, [REGISTER] ), 
+  "POP" =>    Op.new(0x21, [REGISTER] ), 
+  "DROP" =>   Op.new(0x22, [] ), 
+  "ADD" =>    Op.new(0x30, [REGISTER, REGISTER] ), 
+  "ADDI" =>   Op.new(0x31, [REGISTER, VALUE] ), 
+  "ADDR" =>   Op.new(0x32, [REGISTER, REGISTER] ), 
+  "ADDIR" =>  Op.new(0x33, [REGISTER, VALUE] ), 
+  "JMP" =>    Op.new(0x40, [ADDRESS] ), 
+  "JMPIZ" =>  Op.new(0x41, [REGISTER, ADDRESS] ), 
+  "JMPNZ" =>  Op.new(0x42, [REGISTER, ADDRESS] ), 
+  "IFZJMP" => Op.new(0x43, [ADDRESS, ADDRESS] ), 
 }
+
+
+
+def create_args(opc, args)
+  res = ""
+  args.each_with_index do |arg, i| 
+    if @ops[opc].args[i] == REGISTER
+      res << [arg.to_i(16)].pack("S") # pack for 16 bit if register address
+    elsif @ops[opc].args[i] == ADDRESS || @ops[opc].args[i] == VALUE
+      res << [arg.to_i(16)].pack("Q") # pack for 64 bit if adress or value
+    end
+  end
+  res
+end
 
 @code = ""
 
@@ -37,13 +57,13 @@ loop do
   line = gets
   nodes = line.split(" ")
   code = nodes.shift
-  args = nodes.map { |a| [a.to_i(16)].pack("Q").reverse }
+  args = create_args(code, nodes)
 
   break unless @ops.keys.include? code
 
-  puts "Code: #{@ops[code]} #{args.map { |a| a.to_s() }.join('')}"
-  @code << [@ops[code]].pack("Q")
-  args.each do |a| @code << a end
+  puts "Code: #{@ops[code]} #{nodes.join(' ')}"
+  @code << [@ops[code].opc].pack("Q")
+  @code << args
 
 end
 
