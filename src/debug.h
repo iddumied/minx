e13af408c60404d719ff5b8bf7938d0b47fe50fe
Matthias Beyer <matthias@ib-fb.de>
2013-01-11 20:18:42 +0100

    added missing config checks

--- src/debug.h
+++ src/debug.h
@@ -7,24 +7,34 @@
 
 #define minxvmdbgprint(f,...)                                               \
     do {                                                                    \
-        if(minx_config_is_set(CONF_MVM_DEBUGGING)) {                        \
+        ConfigurationValue *cv = minx_config_get(CONF_MVM_DEBUGGING);       \
+        if(cv != NULL && cv->b) {                                           \
             printf("[minx][vm] "f,__VA_ARGS__);                             \
         }                                                                   \
     }while(0)
 
 #define EXPLAIN_FUNCTION()                                                  \
     do{                                                                     \
-        printf("[minx][vm] func: %s, line %s\n", __func__, __LINE__);       \
+        ConfigurationValue *cv = minx_config_get(CONF_SRC_DEBUGGING);       \
+        if(cv != NULL && cv->b) {                                           \
+            printf("[minx][vm] func: %s, line %s\n", __func__, __LINE__);   \
+        }                                                                   \
     } while(0)
 
 #define EXPLAIN_OPCODE(opc)                                                 \
     do {                                                                    \
-        printf("[minx][vm] opcode: %s\n", opc);                             \
+        ConfigurationValue *cv = minx_config_get(CONF_SRC_DEBUGGING);       \
+        if(cv != NULL && cv->b) {                                           \
+            printf("[minx][vm] opcode: %s\n", opc);                         \
+        }                                                                   \
     } while(0)
 
 #define EXPLAIN_OPCODE_WITH(opc,format,...)                                 \
     do {                                                                    \
-        printf("[minx][vm] opcode: %s ("format")\n", opc, __VA_ARGS__);     \
+        ConfigurationValue *cv = minx_config_get(CONF_SRC_DEBUGGING);       \
+        if(cv != NULL && cv->b) {                                           \
+            printf("[minx][vm] opcode: %s ("format")\n", opc, __VA_ARGS__); \
+        }                                                                   \
     } while(0)
 
 #endif //__MINX_DEBUG_H__
