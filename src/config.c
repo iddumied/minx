e1c82ad724f5d65388aa6de0b01d1cab8a9832bd
Matthias Beyer <matthias@ib-fb.de>
2013-01-11 10:59:56 +0100

    added config types and parsing function body

--- src/config.c
+++ src/config.c
@@ -1,6 +1,11 @@
 #include "config.h"
 
 /*
+ * static functions
+ */
+static void         set_config          (ConfigurationType ct);
+
+/*
  * Static variables
  */
 
@@ -8,14 +13,18 @@
  * default config 
  */
 static Configuration    configuration[] = {
-    {   .type = VERBOSITY,  .value.i = 0  },
+    {   .type = CONF_VERBOSITY,     .value.i = 0    },
+    {   .type = CONF_MVM_DEBUGGING, .value.i = 0    },
+    {   .type = CONF_SRC_DEBUGGING, .value.i = 0    },
 };
 
 /*
  * config keys 
  */
 static ConfigurationKeyMap confkeys[] = {
-    {   .type   = VERBOSITY,    .configkey = "-v" },
+    {   .type   = CONF_VERBOSITY,               .configkey = "-v"           },
+    {   .type   = CONF_MVM_DEBUGGING,           .configkey = "--vm-debug"   },
+    {   .type   = CONF_SRC_DEBUGGING,           .configkey = "-d"           },
 };
 
 static unsigned int     number_of_confs;
@@ -36,7 +45,20 @@ void minx_config_shutdown() {
  * parse config 
  */
 void minx_config_parse(unsigned int argc, char ** argv) {
-
+    unsigned int i, j;
+    for( i = 0 ; i < argc; i++ ) {
+        for( j = 0 ; j < (sizeof(*confkeys)/sizeof(confkeys[0])); j++ ) {
+            if( 0 == strcmp( confkeys[j].configkey, argv[i] )) {
+                switch(confkeys[j].type) {
+                    case CONF_VERBOSITY:
+                    case CONF_MVM_DEBUGGING:
+                    case CONF_SRC_DEBUGGING:
+                        set_config(confkeys[j].type);
+                        break;
+                }
+            }
+        }
+    }
 }
 
 void * minx_config_get(ConfigurationType ct) {
@@ -45,4 +67,22 @@ void * minx_config_get(ConfigurationType ct) {
         if(configuration[i].type == ct)
             return &configuration[i].value;
     return NULL;
+}
+
+int minx_config_is_set(ConfigurationType ct) {
+    return minx_config_get(ct) != NULL;
+}
+
+/*
+ *
+ * Static functions
+ *
+ */
+static void set_config(ConfigurationType ct) {
+    unsigned int i;
+    for(i = 0;
+        i < (sizeof(*configuration)/sizeof(configuration[0])) &&
+        configuration[i].type != ct;
+        i++ );
+    configuration[i].value.i = 1;
 }
