8b196f2e7897e3a2deff2b616b782832a0002856
Matthias Beyer <matthias@ib-fb.de>
2013-01-13 12:57:12 +0100

    added some print helper macros

--- src/minx.h
+++ src/minx.h
@@ -1,5 +1,10 @@
 #ifndef __MINX_MAIN_H__
 #define __MINX_MAIN_H__
 
+#define MINX_PRINT_PREFIX               "[minx]"
+#define MINX_VM_PRINT_PREFIX            "[minx][vm]"
+#define MINX_VM_FUNC_PRINT_PREFIX       "[minx][vm][func]"
+#define MINX_VM_OP_PRINT_PREFIX         "[minx][vm][op]"
+
 
 #endif // __MINX_MAIN_H__
