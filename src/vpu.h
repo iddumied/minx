c5413e56c6ae5ef2fa0fa5b9f9402af78959aea8
Matthias Beyer <matthias@ib-fb.de>
2013-01-19 18:35:37 +0100

    added heap addr size

--- src/vpu.h
+++ src/vpu.h
@@ -92,6 +92,7 @@
 #define         OPC_SIZE                2 /*16 bit*/
 #define         VALUE_SIZE              8 /*64 bit*/
 #define         REGISTER_ADDRESS_SIZE   2 /*16 bit*/
+#define         HEAP_ADDRESS_SIZE       2 /*16 bit*/
 #define         REGISTER_MASK           0x0000FFFF /* currently, only take 2 Byte */
 #define         PROGRAM_ADDRESS_SIZE    8 /*64 bit, because program_pointer is 64 bit*/
 
