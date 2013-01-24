793c222cd8ceed442b73f6b913336aaa67211011
Matthias Beyer <matthias@ib-fb.de>
2013-01-24 18:39:08 +0100

    added header for disasm, some structural changes

--- src/disasm.c
+++ src/disasm.c
@@ -1,59 +1,68 @@
-#include "opcodes.h"
-#include "debug.h"
+#include "disasm.h"
 
 static struct {
-    unsigned int    opcode;
     char            *desc;
     unsigned int    params[];
 
 } opcode_map[] = {
-    { OPC_NOP,    "NOP",      { } }, 
-    { OPC_CALL,   "CALL",     { } }, 
-    { OPC_RET,    "RET",      { } }, 
-    { OPC_MOV,    "MOV",      { } }, 
-    { OPC_MOVI,   "MOVI",     { } }, 
-    { OPC_NOT,    "NOT",      { } }, 
-    { OPC_NOTR,   "NOTR",     { } }, 
-    { OPC_AND,    "AND",      { } }, 
-    { OPC_ANDI,   "ANDI",     { } }, 
-    { OPC_ANDR,   "ANDR",     { } }, 
-    { OPC_ANDIR,  "ANDIR",    { } }, 
-    { OPC_OR,     "OR",       { } }, 
-    { OPC_ORI,    "ORI",      { } }, 
-    { OPC_ORR,    "ORR",      { } }, 
-    { OPC_ORIR,   "ORIR",     { } }, 
-    { OPC_DEC,    "DEC",      { } }, 
-    { OPC_INC,    "INC",      { } }, 
-    { OPC_LSH,    "LSH",      { } }, 
-    { OPC_RSH,    "RSH",      { } }, 
-    { OPC_CMP,    "CMP",      { } }, 
-    { OPC_CMPI,   "CMPI",     { } }, 
-    { OPC_EQL,    "EQL",      { } }, 
-    { OPC_EQLI,   "EQLI",     { } }, 
-    { OPC_PUSH,   "PUSH",     { } }, 
-    { OPC_POP,    "POP",      { } }, 
-    { OPC_DROP,   "DROP",     { } }, 
-    { OPC_ADD,    "ADD",      { } }, 
-    { OPC_ADDI,   "ADDI",     { } }, 
-    { OPC_ADDR,   "ADDR",     { } }, 
-    { OPC_ADDIR,  "ADDIR",    { } }, 
-    { OPC_JMP,    "JMP",      { } }, 
-    { OPC_JMPIZ,  "JMPIZ",    { } }, 
-    { OPC_JMPNZ,  "JMPNZ",    { } }, 
-    { OPC_IFZJMP, "IFZJMP",   { } }, 
-    { OPC_PSTACK, "PSTACK",   { } }, 
-    { OPC_PREGS,  "PREGS",    { } }, 
-    { OPC_PPROG,  "PPROG",    { } }, 
-    { OPC_PMEMS,  "PMEMS",    { } }, 
-    { OPC_PMEM,   "PMEM",     { } }, 
-    { OPC_ALLOC,  "ALLOC",    { } }, 
-    { OPC_ALLOCI, "ALLOCI",   { } }, 
-    { OPC_RESIZE, "RESIZE",   { } }, 
-    { OPC_RESIZEI,"RESIZEI",  { } }, 
-    { OPC_FREE,   "FREE",     { } }, 
-    { OPC_PUT,    "PUT",      { } }, 
-    { OPC_READ,   "READ",     { } }, 
-    { OPC_GETSIZE,"GETSIZE",  { } }, 
-}; 
+    [OPC_NOP]       = { "NOP",      { } }, 
+    [OPC_CALL]      = { "CALL",     { } }, 
+    [OPC_RET]       = { "RET",      { } }, 
+    [OPC_MOV]       = { "MOV",      { } }, 
+    [OPC_MOVI]      = { "MOVI",     { } }, 
+    [OPC_NOT]       = { "NOT",      { } }, 
+    [OPC_NOTR]      = { "NOTR",     { } }, 
+    [OPC_AND]       = { "AND",      { } }, 
+    [OPC_ANDI]      = { "ANDI",     { } }, 
+    [OPC_ANDR]      = { "ANDR",     { } }, 
+    [OPC_ANDIR]     = { "ANDIR",    { } }, 
+    [OPC_OR]        = { "OR",       { } }, 
+    [OPC_ORI]       = { "ORI",      { } }, 
+    [OPC_ORR]       = { "ORR",      { } }, 
+    [OPC_ORIR]      = { "ORIR",     { } }, 
+    [OPC_DEC]       = { "DEC",      { } }, 
+    [OPC_INC]       = { "INC",      { } }, 
+    [OPC_LSH]       = { "LSH",      { } }, 
+    [OPC_RSH]       = { "RSH",      { } }, 
+    [OPC_CMP]       = { "CMP",      { } }, 
+    [OPC_CMPI]      = { "CMPI",     { } }, 
+    [OPC_EQL]       = { "EQL",      { } }, 
+    [OPC_EQLI]      = { "EQLI",     { } }, 
+    [OPC_PUSH]      = { "PUSH",     { } }, 
+    [OPC_POP]       = { "POP",      { } }, 
+    [OPC_DROP]      = { "DROP",     { } }, 
+    [OPC_ADD]       = { "ADD",      { } }, 
+    [OPC_ADDI]      = { "ADDI",     { } }, 
+    [OPC_ADDR]      = { "ADDR",     { } }, 
+    [OPC_ADDIR]     = { "ADDIR",    { } }, 
+    [OPC_JMP]       = { "JMP",      { } }, 
+    [OPC_JMPIZ]     = { "JMPIZ",    { } }, 
+    [OPC_JMPNZ]     = { "JMPNZ",    { } }, 
+    [OPC_IFZJMP]    = { "IFZJMP",   { } }, 
+    [OPC_PSTACK]    = { "PSTACK",   { } }, 
+    [OPC_PREGS]     = { "PREGS",    { } }, 
+    [OPC_PPROG]     = { "PPROG",    { } }, 
+    [OPC_PMEMS]     = { "PMEMS",    { } }, 
+    [OPC_PMEM]      = { "PMEM",     { } }, 
+    [OPC_ALLOC]     = { "ALLOC",    { } }, 
+    [OPC_ALLOCI]    = { "ALLOCI",   { } }, 
+    [OPC_RESIZE]    = { "RESIZE",   { } }, 
+    [OPC_RESIZEI]   = { "RESIZEI",  { } }, 
+    [OPC_FREE]      = { "FREE",     { } }, 
+    [OPC_PUT]       = { "PUT",      { } }, 
+    [OPC_READ]      = { "READ",     { } }, 
+    [OPC_GETSIZE]   = {"GETSIZE",   { } }, 
+};
 
+static uint64_t program_pointer;
 
+void minx_disasm_run() {
+    program_pointer = 0x00;
+    uint16_t *opcode;
+    unsigned int i;
+
+    while(minx_binary_exists_at(program_pointer)) {
+        opcode = minx_get_opcode(program_pointer);
+        printf("%s", opcode_map[opcode].desc);
+    }
+}
---
+++ src/disasm.h
@@ -0,0 +1,10 @@
+#ifndef __MINX_DISASM_H__
+#define __MINX_DISASM_H__
+
+#include "opcodes.h"
+#include "debug.h"
+#include "binary_reader.h"
+
+void minx_disasm_run(void);
+
+#endif // __MINX_DISASM_H__
