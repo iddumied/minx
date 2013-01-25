78e063eb6bd14b9cb6400216601649e96997a3a7
Matthias Beyer <matthias@ib-fb.de>
2013-01-25 18:15:15 +0100

    added error code for shutdown

---
+++ src/error.c
@@ -0,0 +1,46 @@
+#include "error.h"
+
+static void (**shutdown_functions)(void);
+static unsigned int shutdown_functions_cnt;
+
+/*
+ * init function for the error module
+ */
+void minx_error_init() {
+    shutdown_functions      = (void(**)(void)) malloc(sizeof(*shutdown_functions));
+    shutdown_functions_cnt  = 0;
+
+    minx_error_register_shutdown_function(minx_error_shutdown);
+}
+
+/*
+ * shutdown function for the error module
+ */
+void minx_error_shutdown() {
+}
+
+/*
+ * shutdown all modules.
+ *
+ * counts down, so last registered shutdown function is called first.
+ * That's good because the shutdown function for the error-module is registered
+ * first!
+ */
+void minx_error_global_shutdown(void) {
+    unsigned int i;
+    for(i = shutdown_functions_cnt; i ; i-- ) {
+        shutdown_functions[i]();
+    }
+}
+
+/*
+ * register shutdown function
+ */
+void minx_error_register_shutdown_function(void (*func)(void)) {
+    shutdown_functions = realloc(shutdown_functions, 
+            sizeof(*shutdown_functions) * shutdown_functions_cnt+1);
+
+    shutdown_functions[shutdown_functions_cnt] = func;
+
+    shutdown_functions_cnt++;
+}
