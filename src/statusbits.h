81055f5eff772e500152ed6a2298f277b1dc50f8
Matthias Beyer <matthias@ib-fb.de>
2013-01-26 11:19:59 +0100

    added new statusbits

--- src/statusbits.h
+++ src/statusbits.h
@@ -3,6 +3,9 @@
 
 #define         OVERFLOW_BIT    0
 #define         ALLOC_BIT       1
-#define         RESIZE_BIT      2
+#define         FREE_BIT        2
+#define         RESIZE_BIT      3
+#define         PUT_BIT         4
+#define         READ_BIT        5
 
 #endif // __MINX_VPU_STATUSBITS_H__
