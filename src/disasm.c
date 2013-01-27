1c7f1bd01c8f56712eac9fd8d1753c1e72a226f6
Matthias Beyer <matthias@ib-fb.de>
2013-01-27 16:43:26 +0100

    implementation of the print_parameters() function and some fixes

--- src/disasm.c
+++ src/disasm.c
@@ -69,20 +69,45 @@ static uint64_t program_pointer;
 
 void minx_disasm_run() {
     program_pointer = 0x00;
-    uint16_t *opcode;
+    uint16_t *opcode = (uint16_t*) malloc(sizeof(uint16_t));
     while(minx_binary_exists_at(program_pointer)) {
         opcode = minx_binary_get_at(program_pointer, OPC_SIZE, opcode, sizeof(*opcode));
         printf("%s", opcode_map[*opcode].desc);
+        program_pointer += OPC_SIZE;
         print_parameters(opcode_map[*opcode].params);
     }
 }
 
 static void print_parameters(unsigned int *params) {
     unsigned int i;
+    uint16_t *param16bit = malloc( sizeof(uint16_t) );
+    uint64_t *param64bit = malloc( sizeof(uint64_t) );
+
+    unsigned int param_size;
+
     for(i = 0; params[i]; i++) {
-        printf("%u", params[i]);
+        printf(" %u: ", i);
+
+        if( params[i] == REGISTER_ADDRESS_SIZE || params[i] == HEAP_ADDRESS_SIZE ) {
+            param16bit = (uint16_t*) minx_binary_get_at(program_pointer, 
+                                                        params[i],
+                                                        param16bit,
+                                                        params[i]);
+            printf("%"PRIu16, param16bit);
+        }
+        else { /* param 8 byte = 64 bit */
+            param64bit = (uint64_t*) minx_binary_get_at(program_pointer,
+                                                        params[i],
+                                                        param64bit,
+                                                        params[i]);
+
+            printf("%"PRIu64, param64bit);
+        }
+        
         if(params[i+1])
-            printf(", ");
+            printf(",");
+
+        program_pointer += params[i];
     }
     printf("\n");
 }
