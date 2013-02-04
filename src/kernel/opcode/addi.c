b3d990a5fbabbe16902747acb93469a143295f7b
Matthias Beyer <matthias@ib-fb.de>
2013-02-04 22:25:48 +0100

    fixed explain-code macro call format

--- src/kernel/opcode/addi.c
+++ src/kernel/opcode/addi.c
@@ -8,14 +8,16 @@
  * Result in akku
  */
 void minx_opc_addi_func(uint64_t *params) {
-#ifdef DEBUGGING
-    EXPLAIN_OPCODE("reg %"PRIu64" + %"PRIu64, params[0], params[1]);
-#endif
-
+    
     Register *status = minx_registers_find_register(statusregister);
     Register *r1 = minx_registers_find_register(akku_register_number);
-
     Register *r2 = minx_registers_find_register(params[0]);
+
+#ifdef DEBUGGING
+    EXPLAIN_OPCODE("R%"PRIu64" (%"PRIu64") + %"PRIu64, 
+            params[0], r2->value,
+            params[1]);
+#endif
 
     if (minx_util_check_addition_overflow64(r2->value, params[1])) {
         setbit(status->value, OVERFLOW_BIT);
