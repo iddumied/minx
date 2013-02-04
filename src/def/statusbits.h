b0efbfe736106991f19700bd28cffea03e418213
Matthias Beyer <matthias@ib-fb.de>
2013-02-04 17:56:57 +0100

    added some comments for statusbits

--- src/def/statusbits.h
+++ src/def/statusbits.h
@@ -5,12 +5,12 @@ typedef enum {
     OVERFLOW_BIT = 0,
     ZERO_BIT,
     DIVZERO_BIT,
-    ALLOC_BIT,
-    FREE_BIT,
-    RESIZE_BIT,
-    PUT_BIT,
-    READ_BIT,
-    NO_RAND,
+    ALLOC_BIT, /* set if alloc didn't work */
+    FREE_BIT, /* set if free didn't work */
+    RESIZE_BIT, /* set if resize didn't work */
+    PUT_BIT, /* set if put didn't work */
+    READ_BIT, /* set if read didn't work */
+    NO_RAND, /* set if there is no random fetched */
 } StatusBits;
 
 #endif // __MINX_VPU_STATUSBITS_H__
