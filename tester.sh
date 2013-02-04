55f4be68bc34787a5b69c4cea02d06b91096bd46
Matthias Beyer <matthias@ib-fb.de>
2013-02-04 22:27:16 +0100

    removed output error in tester script

--- tester.sh
+++ tester.sh
@@ -3,7 +3,7 @@ for file in $(ls asm | grep out | grep -v endless); do
 	echo -n "Run $file"
 	./bin/minx asm/$file 
 	if [[ $? -ne 0 ]]; then
-		echo "\t:: error with $file"
+		echo " :: error with $file"
 	else
 		echo ""
 	fi
