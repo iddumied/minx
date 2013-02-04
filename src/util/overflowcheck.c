aae6a0fe3fee035bf911b700283498093efc860f
Matthias Beyer <matthias@ib-fb.de>
2013-02-04 14:34:37 +0100

    removed implementation
    of minx_util_check_multiplication_obverflow64() function, as I don't
    know if it works.

--- src/util/overflowcheck.c
+++ src/util/overflowcheck.c
@@ -9,22 +9,11 @@ int minx_util_check_subtraction_overflow64(uint64_t a, uint64_t b) {
 }
 
 int minx_util_check_multiplication_overflow64(uint64_t a, uint64_t b) {
-    if( a > b ) return minx_util_check_multiplication_overflow64(b, a);
-
-    if( a > UINT32_MAX && b > 1 ) return 1; /* overflow */
-
-    uint32_t c = b >> 32;
-    uint32_t d = UINT32_MAX & b;
-    uint64_t r = a * c;
-    uint64_t s = a * d;
-
-    if( s > UINT32_MAX ) return 1; /* overflow */
-
-    s <<= 32;
-
-    return 0; /* no overflow */
+    /* not implemented yet */
+    return 1;
 }
 
 int minx_util_check_power_overflow64(uint64_t a, uint64_t b) {
     /* not implemented yet */
+    return 1;
 }
