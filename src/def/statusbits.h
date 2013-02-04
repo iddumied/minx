2ed866560ec48e4bb6d8104bc3e0cf479f0de484
Matthias Beyer <matthias@ib-fb.de>
2013-02-04 14:35:28 +0100

    added statusbits

--- src/def/statusbits.h
+++ src/def/statusbits.h
@@ -8,5 +8,7 @@
 #define         PUT_BIT         4
 #define         READ_BIT        5
 #define         DIVZERO_BIT     6
+#define         ZERO_BIT        7
+#define         NO_RAND         8
 
 #endif // __MINX_VPU_STATUSBITS_H__
