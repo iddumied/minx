598d3ef02b0232d652a1cde85ba77950acd0b598
Matthias Beyer <matthias@ib-fb.de>
2013-02-07 11:57:19 +0100

    fixed ugly negation error

--- src/util/macros.h
+++ src/util/macros.h
@@ -2,6 +2,6 @@
 #define __MINX_UTIL_MACROS_H__
 
 #define         setbit(byte,bitnum)     do { byte |=  0x0001<<bitnum; } while(0)
-#define         clrbit(byte,bitnum)     do { byte &= !(0x0001<<bitnum); } while(0)
+#define         clrbit(byte,bitnum)     do { byte &= ~(0x0001<<bitnum); } while(0)
 
 #endif // __MINX_UTIL_MACROS_H__
