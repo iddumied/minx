61b637c01d4a2597f16911532c5664b66d0fd14a
Matthias Beyer <matthias@ib-fb.de>
2013-02-18 23:18:00 +0100

    fixed debug output

--- src/kernel/opcode/ldaddr.c
+++ src/kernel/opcode/ldaddr.c
@@ -6,14 +6,15 @@
  * Affects Program Pointer: NO
  *
  */
-void minx_opc__func(uint64_t *params) {
-#ifdef DEBUGGING
-    EXPLAIN_OPCODE("load address of next opcode into akku");
-#endif
-
+void minx_opc_ldaddr_func(uint64_t *params) {
     Register *prop = minx_registers_find_register(program_pointer_register_number);
     Register *akku = minx_registers_find_register(akku_register_number);
 
     akku->value = prop->value + OPC_SIZE;
+
+#ifdef DEBUGGING
+    EXPLAIN_OPCODE("load address of next opcode into akku (%"PRIu64")", akku->value);
+#endif
+
 }
 
