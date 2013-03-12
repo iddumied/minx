eafaf243ba8aa6b4ec364e7b4b084dc0d5680030
Matthias Beyer <matthias@ib-fb.de>
2013-03-12 15:42:32 +0100

    FIX: pushed a value instead of a pointer
    fixed by allocating a single uint64_t and put the value inside. Gets
    free'd when it is poped from stack.

--- src/kernel/opcode/call.c
+++ src/kernel/opcode/call.c
@@ -22,8 +22,12 @@ void minx_opc_call_func(uint64_t *params) {
      * push the address of the next opcode to the stack,
      * the address has size (PROGRAM_ADDRESS_SIZE) (in this case this is 8 Byte)
      */
+
+    uint64_t *data = (uint64_t*) malloc(sizeof(uint64_t));
+    *data = minx_kernel_program_pointer_get() + OPC_SIZE + PROGRAM_ADDRESS_SIZE;
+
     stackpush(  stack,
-                (void*)(minx_kernel_program_pointer_get() + OPC_SIZE + PROGRAM_ADDRESS_SIZE), 
+                (void*)data, 
                 (size_t)PROGRAM_ADDRESS_SIZE);
 
     minx_kernel_program_pointer_manipulate(params[0]);
