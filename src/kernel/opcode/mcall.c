0c95257578e1805af6f1c6092b42de143cb8b2b2
Matthias Beyer <matthias@ib-fb.de>
2013-03-12 14:35:14 +0100

    FIX: find heap by register, not constant

--- src/kernel/opcode/mcall.c
+++ src/kernel/opcode/mcall.c
@@ -9,7 +9,9 @@
 void minx_opc_mcall_func(uint64_t *params) {
     Register *mod_id_reg = minx_registers_find_register(params[0]);
     Register *mod_op_reg = minx_registers_find_register(params[1]);
-    HeapNode *heap = minx_kernel_heap_get(params[2]);
+
+    Register *r1 = minx_registers_find_register(params[2]);
+    HeapNode *heap = minx_kernel_heap_get(r1->value);
 
 #ifdef DEBUGGING
     EXPLAIN_OPCODE_WITH_HEXF(
