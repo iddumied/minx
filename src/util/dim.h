987fae12effefccd035918f152cc6fd446e000e2
Matthias Beyer <matthias@ib-fb.de>
2013-03-19 22:48:11 +0100

    added macro dimension header file

---
+++ src/util/dim.h
@@ -0,0 +1,19 @@
+#ifndef __MINX_DIM_H__
+#define __MINX_DIM_H__
+
+/*
+ *
+ * Defining *byte sizes here
+ *
+ */
+
+#define BYTE        1
+#define KILOBYTE    (1000 * BYTE)
+#define MEGABYTE    (1000 * KILOBYTE)
+#define GIGABYTE    (1000 * MEGABYTE)
+
+#define KIBIBYTE    (1024 * BYTE)
+#define MEBIBYTE    (1024 * KIBIBYTE)
+#define GIBIBYTE    (1024 * MEBIBYTE)
+
+#endif // __MINX_DIM_H__
