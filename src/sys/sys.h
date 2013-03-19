a42108d28fdf40f6e7503f065c1eac21bbffcddf
Matthias Beyer <matthias@ib-fb.de>
2013-03-19 22:48:32 +0100

    added os-interface-code including subdir

---
+++ src/sys/sys.h
@@ -0,0 +1,15 @@
+#ifndef __MINX_SYS_H__
+#define __MINX_SYS_H__
+
+/*
+ *
+ *
+ * System interface functions
+ *
+ *
+ */
+
+long        minx_sys_get_ramsize        (void);
+
+
+#endif //__MINX_SYS_H__
