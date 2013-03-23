a8e95e00f409bcefd79fabbe3b7728f7df06a520
Matthias Beyer <matthias@ib-fb.de>
2013-03-23 23:19:45 +0100

    added debugging macros

--- src/util/debug.h
+++ src/util/debug.h
@@ -7,13 +7,30 @@
 
 #include "util/print_prefixes.h"
 
-#define minxkerneldbgprintf(f,...)                                          \
+/* helper macro */
+#define minxdbgprintf(conf,pref,format,...)                                 \
+    do {                                                                    \
+        ConfigurationValue *cv = minx_config_get(conf);                     \
+        if(cv != NULL && cv->b) {                                           \
+            printf(pref": "format,__VA_ARGS__);                             \
+        }                                                                   \
+    }while(0)
+
+/* debugging for binary */
+#define minxbinarydbgprintf(f,...)                                          \
+    minxdbgprintf(CONF_MINX_DEBUGGING,MINX_BINARY_PRINT_PREFIX,f,__VA_ARGS__);
+
+#define minxbinarydbgprint(f)                                               \
     do {                                                                    \
         ConfigurationValue *cv = minx_config_get(CONF_MINX_DEBUGGING);      \
         if(cv != NULL && cv->b) {                                           \
-            printf(MINX_KERNEL_PRINT_PREFIX": "f,__VA_ARGS__);              \
+            printf(MINX_BINARY_PRINT_PREFIX": "f);                          \
         }                                                                   \
     }while(0)
+
+/* debugging for kernel */
+#define minxkerneldbgprintf(f,...)                                          \
+    minxdbgprintf(CONF_MINX_DEBUGGING,MINX_KERNEL_PRINT_PREFIX,f,__VA_ARGS__);
 
 #define minxkerneldbgprint(f)                                               \
     do {                                                                    \
@@ -23,6 +40,7 @@
         }                                                                   \
     }while(0)
 
+/* source debugging */
 #define MINX_SRC_DEBUG(module,f,...)                                        \
     do {                                                                    \
         ConfigurationValue *cv = minx_config_get(CONF_SRC_DEBUGGING);       \
@@ -40,6 +58,7 @@
         }                                                                   \
     } while(0)
 
+/* opcodes */
 #define EXPLAIN_OPCODE(format,...)                                          \
     do {                                                                    \
         if(minx_config_get(CONF_SRC_DEBUGGING)->b) {                        \
